865581f897863d95b251c30242a6004c88bfefd6
barshefet <bar.shefet@gmail.com>
2023-07-26 15:38:55 +0300

    started on the Films page

--- src/client/src/components/Films/Films.tsx
+++ src/client/src/components/Films/Films.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navmenu from "../Navmenu/Navmenu";
+import Videos from "./videos";
+import './Films.scss'
 
 const Films = () => {
-  return <div>Films</div>;
+  return (
+    <>
+      <div className="films-container">
+        <Navmenu />
+        <h1>Films</h1>
+        <Videos />
+      </div>
+    </>
+  );
 };
 
 export default Films;
---
+++ src/client/src/components/Films/Videos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Videos.scss'
+
+const Videos = () => {
+  return (
+    <div className='videos-container'>
+        <h1>videos</h1>
+        </div>
+  )
+}
+
+export default Videos
