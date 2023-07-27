9190c12fba048784e8fcc2b5e92c0893f5bb52b8
barshefet <bar.shefet@gmail.com>
2023-07-27 11:31:18 +0300

    site is done

--- src/client/src/components/Contact/Contact.tsx
+++ src/client/src/components/Contact/Contact.tsx
@@ -8,9 +8,15 @@ const Contact = () => {
       <div className="contact-container">
         <h1>Let's get in touch!</h1>
         <div className="contact-information">
-           <div className="digits"> <h2>+972 50 338 3090</h2></div>
+          <div className="digits">
+            <h2>+972 50 338 3090</h2>
             <h2>bar.shefet@gmail.com</h2>
-          <img className="frame" src="images/frame.jpg" />
+            <div className="icons">
+            <a href="https://www.instagram.com/bar_shefet/"><img className="icon1" src="images/insticon.png" /></a>
+            <a href="https://github.com/barshefet"><img className="icon2" src="images/githublogo.png" alt="" /></a>
+            </div>
+          </div>
+          <img className="frame" src="images/frame.png" />
           <img className="painting" src="images/girl-balloon.png" />
         </div>
       </div>
