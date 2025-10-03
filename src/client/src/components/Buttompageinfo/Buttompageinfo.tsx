502f09d963335dacffdc6b2ea201dfc6cc00a1f2
barshefet <bar.shefet@gmail.com>
2025-10-04 00:45:35 +0300

    fix: refactor Buttompageinfo component structure and enhance styles for better layout and responsiveness

--- src/client/src/components/Buttompageinfo/Buttompageinfo.tsx
+++ src/client/src/components/Buttompageinfo/Buttompageinfo.tsx
@@ -3,6 +3,12 @@ import "./Buttompageinfo.scss";
 const Buttompageinfo = () => {
   return (
     <>
+    <div className="info-container">
+      <div className="contactme-container">
+        <h3>Get In Touch</h3>
+        <h3>Click To Copy</h3>
+        <h1 className="email"><span>bar.shefet@gmail.com</span></h1>
+      </div>
       <div className="info">
         <div className="left-info">
           <h4>bar.shefet@gmail.com</h4>
@@ -11,16 +17,17 @@ const Buttompageinfo = () => {
         <div className="middle-info">
             <div className="insta">
             <a href="https://www.instagram.com/bar_shefet/" target="_blank" rel="noopener noreferrer">
-              <img src="images/instagram.png" alt="Instagram" />
+              {"Instagram"}
             </a>
-            </div>
+          </div>
           <div className="youtube">
             <a href="https://www.youtube.com/@bar4490" target="_blank" rel="noopener noreferrer">
-              <img src="images/youtube.png" alt="YouTube" />
+              {"YouTube"}
             </a>
             </div>
         </div>
         <div className="right-info"><h4>+972 50 338 3090</h4></div>
+      </div>
       </div>
       <div className="rights">
         <p>©2025 by Bar Shefet. Programed by Bar Shefet.</p>
