fb48ccf83f04d74ba3bb1aa614b7aa9363861671
barshefet <bar.shefet@gmail.com>
2025-10-04 14:58:58 +0300

    fix: replace anchor tags with navigation divs in bottomHome component for improved routing

--- src/client/src/components/Homepage/bottomHome.tsx
+++ src/client/src/components/Homepage/bottomHome.tsx
@@ -1,19 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 import "./bottomHome.scss";
 
 const bottomHome = () => {
+  const nav = useNavigate();
   return (
     <>
       <div className="bottom-home">
         <div className="director">
           <h1>Director</h1>
-          <a href="./Films" className="seeButton">See Portfolio</a>
+          <div onClick={() => nav("./Films")} className="seeButton">
+            See Portfolio
+          </div>
         </div>
         <div className="and">
           <h1>&</h1>
         </div>
         <div className="cinematographer">
-            <h1>Cinematographer</h1>
-          <a href="./Cinematography" className="seeButton">See Showreel</a>
+          <h1>Cinematographer</h1>
+          <div onClick={() => nav("./Cinematography")} className="seeButton">
+            See Showreel
+          </div>
         </div>
       </div>
     </>
